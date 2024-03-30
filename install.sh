1051f68751dbec16878e92ba981e48e8f3eca1c4
Ben Mackereth <ben.mackereth@hotmail.co.uk>
2024-03-30 19:56:42 +0000

    add docker compose

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-# Pull the Docker image and run it
+# Check if Docker is installed
+if ! command -v docker &> /dev/null
+then
+    echo "Docker is not installed. Installing Docker..."
+    
+    # Install Docker using official Docker installation script
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+else
+    echo "Docker is installed."
+fi
+
+# Pull the Docker image
 echo "Pulling Docker image..."
 docker pull strangebee/thehive
+
+# Start the Docker container
 echo "Starting Docker container..."
 docker run -d strangebee/thehive
